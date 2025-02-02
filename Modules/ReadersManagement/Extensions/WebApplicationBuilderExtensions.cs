ae13647851c6ac1f981e4331e478e948a1c76cd2
Nikita <35460261+SnowPowerCore@users.noreply.github.com>
2025-02-02 09:46:14 +0300

    - State init;

--- Modules/ReadersManagement/Extensions/WebApplicationBuilderExtensions.cs
+++ Modules/ReadersManagement/Extensions/WebApplicationBuilderExtensions.cs
@@ -1,5 +1,9 @@
+using MediatR.Pipeline;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using TimeWarp.Features.Persistence;
 using TimeWarp.State;
+using TimeWarp.State.Plus;
 
 namespace snowcoreBlog.Frontend.ReadersManagement.Extensions;
 
@@ -8,6 +12,8 @@ public static class WebApplicationBuilderExtensions
     public static WebAssemblyHostBuilder AddReadersManagement(this WebAssemblyHostBuilder builder)
     {
         builder.Services.AddTimeWarpState();
+        builder.Services.AddTransient(typeof(IRequestPostProcessor<,>), typeof(PersistentStatePostProcessor<,>));
+        builder.Services.AddScoped<IPersistenceService, PersistenceService>();
         return builder;
     }
 }
