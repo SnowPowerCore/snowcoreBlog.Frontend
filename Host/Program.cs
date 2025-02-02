79696b60d64eaf26b3c03a75a1a0a820c02fcf18
Nikita <35460261+SnowPowerCore@users.noreply.github.com>
2025-02-02 14:38:05 +0300

    - Attempt to fix;

--- Host/Program.cs
+++ Host/Program.cs
@@ -1,5 +1,3 @@
-using Apizr;
-using Apizr.Logging;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -8,22 +6,26 @@ using Morris.Blazor.Validation;
 using snowcoreBlog.Frontend.Host;
 using snowcoreBlog.Frontend.Infrastructure.Providers;
 using snowcoreBlog.Frontend.ReadersManagement.Extensions;
+using snowcoreBlog.Frontend.SharedComponents.Extensions;
+using snowcoreBlog.PublicApi.Api;
 using snowcoreBlog.PublicApi.Utilities.Api;
+using TimeWarp.State;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddTimeWarpState(options =>
+{
+    options.Assemblies = [
+        typeof(snowcoreBlog.Frontend.ReadersManagement.Extensions.WebApplicationBuilderExtensions).Assembly,
+        typeof(snowcoreBlog.Frontend.SharedComponents.Extensions.WebApplicationBuilderExtensions).Assembly,
+        typeof(TimeWarp.State.Plus.AssemblyMarker).Assembly
+    ];
+});
 builder.AddReadersManagement();
-builder.Services.AddApizr(
-    static registry => { },
-    static config => config
-        .ConfigureHttpClientBuilder(static builder => builder.AddStandardResilienceHandler())
-        .WithLogging(
-            HttpTracerMode.ExceptionsOnly,
-            HttpMessageParts.ResponseAll,
-            LogLevel.Error)
-);
+builder.AddSharedComponents();
+builder.Services.ConfigureSnowcoreBlogBackendReadersManagementApizrManagers(default);
 builder.Services.AddFluentUIComponents();
 builder.Services.AddFormValidation(static config =>
     config.AddFluentValidation(typeof(ApiResponse).Assembly));
--- Modules/SharedComponents/Extensions/WebApplicationBuilderExtensions.cs
+++ Modules/SharedComponents/Extensions/WebApplicationBuilderExtensions.cs
@@ -1,13 +1,11 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using TimeWarp.State;
 
 namespace snowcoreBlog.Frontend.SharedComponents.Extensions;
 
 public static class WebApplicationBuilderExtensions
 {
-    public static WebAssemblyHostBuilder AddReadersManagement(this WebAssemblyHostBuilder builder)
+    public static WebAssemblyHostBuilder AddSharedComponents(this WebAssemblyHostBuilder builder)
     {
-        builder.Services.AddTimeWarpState();
         return builder;
     }
 }
