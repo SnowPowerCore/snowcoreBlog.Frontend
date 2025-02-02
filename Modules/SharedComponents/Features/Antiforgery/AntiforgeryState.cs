43dbcc9a6238f8c9e853857689ac0ba4e2d4f46c
Nikita <35460261+SnowPowerCore@users.noreply.github.com>
2025-02-02 13:27:53 +0300

    - Antiforgery;

---
+++ Modules/SharedComponents/Features/Antiforgery/AntiforgeryState.cs
@@ -0,0 +1,15 @@
+using TimeWarp.Features.Persistence;
+using TimeWarp.State;
+
+namespace snowcoreBlog.Frontend.SharedComponents.Features.Antiforgery;
+
+[PersistentState(PersistentStateMethod.SessionStorage)]
+public sealed partial class AntiforgeryState : State<AntiforgeryState>
+{
+    public string RequestVerificationToken { get; private set; }
+
+    public override void Initialize()
+    {
+        RequestVerificationToken = string.Empty;
+    }
+}
