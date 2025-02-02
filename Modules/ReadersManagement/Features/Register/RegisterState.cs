491d5be83542b3eb9d0f1888ebb0403bc43d9313
Nikita <35460261+SnowPowerCore@users.noreply.github.com>
2025-02-02 09:46:14 +0300

    - State init;

---
+++ Modules/ReadersManagement/Features/Register/RegisterState.cs
@@ -0,0 +1,17 @@
+using snowcoreBlog.PublicApi.BusinessObjects.Dto;
+using TimeWarp.Features.Persistence;
+using TimeWarp.State;
+
+namespace snowcoreBlog.Frontend.ReadersManagement.Features.Register;
+
+[PersistentState(PersistentStateMethod.SessionStorage)]
+public sealed partial class RegisterState : State<RegisterState>
+{
+    public RequestCreateReaderAccountDto CurrentModel { get; private set; }
+
+    public override void Initialize()
+    {
+        CurrentModel =
+            new() { Email = string.Empty, FirstName = string.Empty, NickName = string.Empty, InitialEmailConsent = false, ConfirmedAgreement = false };
+    }
+}
