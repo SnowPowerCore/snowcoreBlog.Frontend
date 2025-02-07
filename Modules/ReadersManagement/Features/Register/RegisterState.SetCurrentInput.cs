4abf73379bc796661ba87b54a4a7be9daecb9983
Nikita <35460261+SnowPowerCore@users.noreply.github.com>
2025-02-07 23:08:15 +0300

    - Temp user registration;

---
+++ Modules/ReadersManagement/Features/Register/RegisterState.SetCurrentInput.cs
@@ -0,0 +1,33 @@
+using snowcoreBlog.PublicApi.BusinessObjects.Dto;
+using TimeWarp.State;
+
+namespace snowcoreBlog.Frontend.ReadersManagement.Features.Register;
+
+partial class RegisterState
+{
+    public static class SetCurrentInputActionSet
+    {
+        public sealed class Action : IAction
+        {
+            public RequestCreateReaderAccountDto? Request { get; }
+
+            public Action(RequestCreateReaderAccountDto? request)
+            {
+                Request = request;
+            }
+        }
+
+        public sealed class Handler : ActionHandler<Action>
+        {
+            public Handler(IStore store) : base(store) { }
+
+            private RegisterState RegisterState => Store.GetState<RegisterState>();
+
+            public override Task Handle(Action action, CancellationToken cancellationToken)
+            {
+                RegisterState.CurrentModel = action.Request;
+                return Task.CompletedTask;
+            }
+        }
+    }
+}
