c06fd82145196a40db7e1050efc30bbc4295e3df
Nikita <35460261+SnowPowerCore@users.noreply.github.com>
2025-03-31 22:37:21 +0300

    - Extracting component & fixing form validation issues;

---
+++ Modules/SharedComponents/Providers/TrackFieldsChangeProvider.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace snowcoreBlog.Frontend.SharedComponents.Providers;
+
+public class TrackFieldsChangeProvider : ComponentBase, IDisposable
+{
+    [CascadingParameter] private EditContext? CurrentEditContext { get; set; }
+
+    [Parameter] public EventCallback<FieldChangedEventArgs> OnFieldChanged { get; set; } = EventCallback<FieldChangedEventArgs>.Empty;
+
+    protected override void OnInitialized()
+    {
+        if (CurrentEditContext == null)
+        {
+            throw new InvalidOperationException($"{nameof(TrackFieldsChangeProvider)} requires a cascading " +
+                                                $"parameter of type {nameof(EditContext)}. For example, you can use {nameof(TrackFieldsChangeProvider)} " +
+                                                $"inside an {nameof(EditForm)}.");
+        }
+
+        CurrentEditContext.OnFieldChanged += EditContextOnFieldChanged;
+    }
+
+    private void EditContextOnFieldChanged(object? sender, FieldChangedEventArgs args) =>
+        _ = OnFieldChanged.InvokeAsync(args);
+        
+    void IDisposable.Dispose()
+    {
+        if (CurrentEditContext is not default(EditContext))
+            CurrentEditContext.OnFieldChanged -= EditContextOnFieldChanged;
+    }
+}
